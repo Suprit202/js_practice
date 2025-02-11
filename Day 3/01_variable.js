3856ee3452ee9dae2059bf13a1c3560fc7cee5d7
Jeetmhnt <mohantysuprit703@gmail.com>
2025-02-11 19:47:31 +0530

    Day 3 and Day 4 complited

---
+++ Day 3/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "supritmhnt123@gmail.com"
+var accoutPassword = "12345678"
+accountCity = "Mumbai"
+let accountState;
+
+// prefer not to use var due to scope issues
+
+// accountId = 2 //can't change the value of constant variable
+accoutPassword = "87654321"
+accountCity = "Pune"
+
+
+// console.log(accountId);
+console.table([accountId, accountEmail, accoutPassword, accountCity, accountState]) //table is user to display data in tabular form
---
+++ Day 4/01_dataType.js
@@ -0,0 +1,35 @@
+"use Strict"; //treat all js code as newer version
+
+
+// alert("Hello, World!"); //alert is used to display a message in a dialog box (can't be used in node.js)
+// only used in browser
+
+
+// alert(3 + 3);
+
+
+console.log(3+3); console.log("suprit") // ';' separates two statements
+//but it's not a good practice: (code redability is lost)
+
+let name = "Suprit"
+let age = 21
+let isLoggedIn = false
+
+                                          /* data type */
+// Number => 2^53
+// BigInt => 2^53+
+// String => " " or ' '
+// Boolean => true/false
+// null => empty value
+// undefined => variable declared but not assigned
+// Symbol => unique value
+
+                                          /* non-premtive */
+// Object => key-value pair
+// Function => block of code
+// Array => list of values
+// Date => date and time
+
+console.log(typeof age) //typeof is used to check the data type of a variable (number)
+console.log(typeof null) //Object
+console.log(typeof undefined) //undefined
