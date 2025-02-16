3b17aad8744380d72f6acee7a89cab01ba68b848
Suprit202 <supritmhnt123@gmail.com>
2025-02-16 16:21:45 +0530

    controlFlow,switch and truthy value complited

---
+++ Day 8/02_switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+
+let month = "march"  //Invalid month
+month = "mar"   //March
+
+switch (month) {
+  case "jan":
+    console.log("January");    
+    break;
+
+  case "feb":
+    console.log("February");   
+    break;
+
+  case "mar":
+    console.log("March");
+    break;
+
+  case "apr":
+    console.log("April");
+    break;
+
+  case "may":
+    console.log("May");
+    break;
+
+  default:
+    console.log("Invalid month");
+    break;
+}
